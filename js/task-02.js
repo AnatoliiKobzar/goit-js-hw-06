89de16859474ffe977192aee1f36093b758e1617
KobzarAnatolii <TohaPirat@gmail.com>
2022-12-06 18:03:08 +0200

    Done task2

--- js/task-02.js
+++ js/task-02.js
@@ -1,8 +1,29 @@
-const ingredients = [
-  'Potatoes',
-  'Mushrooms',
-  'Garlic',
-  'Tomatos',
-  'Herbs',
-  'Condiments',
-];
+const ingredients = ['Potatoes', 'Mushrooms', 'Garlic', 'Tomatos', 'Herbs', 'Condiments'];
+
+const list = document.querySelector('#ingredients');
+
+const ingredientsEl = [];
+
+// for (const ingredient of ingredients) {
+//   const itemEl = document.createElement('li');
+//   itemEl.textContent = ingredient;
+//   itemEl.classList.add('item');
+//   ingredientsEl.push(itemEl);
+// }
+// // console.log(ingredientsEl);
+
+// list.append(...ingredientsEl);
+
+function addMarkup(array) {
+  for (const ingredient of array) {
+    const itemEl = document.createElement('li');
+    itemEl.textContent = ingredient;
+    itemEl.classList.add('item');
+    ingredientsEl.push(itemEl);
+  }
+  // console.log(ingredientsEl);
+
+  list.append(...ingredientsEl);
+}
+
+addMarkup(ingredients);
