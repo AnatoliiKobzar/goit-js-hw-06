12dc54cb073ef6dbc6c8eb2c02a24a1022d49746
KobzarAnatolii <TohaPirat@gmail.com>
2022-12-08 21:48:13 +0200

    Done task9

--- js/task-09.js
+++ js/task-09.js
@@ -1,3 +1,13 @@
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
 }
+
+const btnEl = document.querySelector('.change-color');
+const textEl = document.querySelector('.color');
+
+btnEl.addEventListener('click', onBtnClick);
+
+function onBtnClick() {
+  textEl.textContent = getRandomHexColor();
+  document.body.style.backgroundColor = textEl.textContent;
+}
