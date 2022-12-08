3e89e7208f0c4342548b080da7fc2868f427b9f4
KobzarAnatolii <TohaPirat@gmail.com>
2022-12-08 21:36:01 +0200

    Done task8

--- js/task-08.js
+++ js/task-08.js
@@ -1,1 +1,24 @@
+const formEl = document.querySelector('.login-form');
 
+formEl.addEventListener('submit', onSubmitForm);
+
+function onSubmitForm(event) {
+  event.preventDefault();
+
+  const {
+    elements: { email, password },
+  } = event.currentTarget;
+
+  if (email.value === '' || password.value === '') {
+    alert('Please fill in all the fields!');
+  }
+
+  const dataForm = {};
+
+  dataForm[event.currentTarget.elements[0].name] = event.currentTarget.elements[0].value;
+  dataForm[event.currentTarget.elements[1].name] = event.currentTarget.elements[1].value;
+
+  console.log(dataForm);
+
+  formEl.reset();
+}
