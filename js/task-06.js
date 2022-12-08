0dfef59f398f027d4a6283dd10fdf8abe2260567
KobzarAnatolii <TohaPirat@gmail.com>
2022-12-08 19:02:31 +0200

    Done task6

---
+++ js/task-06.js
@@ -0,0 +1,13 @@
+const inputEl = document.querySelector('#validation-input');
+
+inputEl.addEventListener('blur', onBlur);
+
+function onBlur(event) {
+  if (event.currentTarget.value.length === Number(event.currentTarget.dataset.length)) {
+    inputEl.classList.add('valid');
+    inputEl.classList.remove('invalid');
+  } else if (event.currentTarget.value.length !== Number(event.currentTarget.dataset.length)) {
+    inputEl.classList.add('invalid');
+    inputEl.classList.remove('valid');
+  } 
+}
