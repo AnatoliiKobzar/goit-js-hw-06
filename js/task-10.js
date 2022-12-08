f2fe52d0ab2505a7b9a44af2f70ce8a5ac508997
KobzarAnatolii <TohaPirat@gmail.com>
2022-12-09 00:15:35 +0200

    Done task10

--- js/task-10.js
+++ js/task-10.js
@@ -1,3 +1,38 @@
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
 }
+
+const btnCreateEl = document.querySelector('[data-create]');
+const btnDestroyEl = document.querySelector('[data-destroy]');
+const inputEl = document.querySelector('[type="number"]');
+const boxWithBoxesEl = document.querySelector('#boxes');
+
+btnCreateEl.addEventListener('click', onBtnCreateClick);
+
+function onBtnCreateClick() {
+  createBoxes(inputEl.value);
+}
+
+let boxes = [];
+
+function createBoxes(quantity) {
+  for (let i = 0; i < quantity; i += 1) {
+    const itemEl = document.createElement('div');
+    itemEl.style.width = `${20 + i * 10}px`;
+    itemEl.style.height = `${20 + i * 10}px`;
+    itemEl.style.backgroundColor = getRandomHexColor();
+    boxes.push(itemEl);
+  }
+  boxWithBoxesEl.append(...boxes);
+}
+
+btnDestroyEl.addEventListener('click', onBtnDestroyClick);
+
+function onBtnDestroyClick() {
+  destroyBoxes();
+}
+
+function destroyBoxes() {
+  boxes = [];
+  boxWithBoxesEl.innerHTML = '';
+}
